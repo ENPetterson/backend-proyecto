3cbe0f271dfe118d7b368f3135fdba9d35d9d8bc
ENPetterson <e.n.petterson@gmail.com>
2023-07-15 16:59:35 -0300

    auth laravel api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function funLogin(Request $request) {
+        //validar
+        //verificar        
+        $credenciales = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+
+        ]);
+
+        if (!Auth::attempt($credenciales)){
+            //generar token
+            return response()->json(["message" => "no autenticado"]);
+        }
+
+        $usuario = Auth::user();
+        $token = $usuario->createToken("token test")->plainTextToken;
+
+        //responder
+        return response()->json([
+            "access_token" => $token,
+            "type_token" => "Bearer",
+            "usuario" => $usuario
+        ]);
+    }
+
+    public function funRegistro(Request $request) {
+        //validar datos
+        $request ->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required",
+            "c_password" => "required|same:password"
+
+        ]);
+
+        //guardar usuario
+        $usuario = new User;
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = bcrypt($request->password);
+        $usuario->save();
+
+        return response()->json(["mensaje" => "Usuario Registrado"], 201);
+    }
+
+    public function funPerfil() {
+        return Auth::user();
+    }
+
+    public function funSalir() {
+        Auth::user()->tokens()->delete();
+
+        return response()->json(["message" => "Salio"]);
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// rutas auth
+Route::prefix('/v1/auth')->group(function(){
+    
+    Route::post('/login', [AuthController::class, "funLogin"]);
+    Route::post('/register', [AuthController::class, "funRegistro"]);
+
+    Route::middleware('auth:sanctum')->group(function(){
+
+        Route::get('/perfil', [AuthController::class, "funPerfil"]);
+        Route::post('/logout', [AuthController::class, "funSalir"]);
+
+    });
+
+});
+
+
